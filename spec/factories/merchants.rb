ddb34903d8efeecfff792303d62b606e7c9918e3
Stephen Malek <malek.stephen44@gmail.com>
2018-08-13 17:39:53 -0600

    add faker data

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :merchant do
-    name "MyString"
+    name { Faker::StarWars.character }
+    created_at { Faker::Time.between(4.days.ago, Time.now, :all) }
+    updated_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 end
