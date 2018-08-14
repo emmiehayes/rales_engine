6fdfbdd35d814ef47b490756ab9290b02efce0ad
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-14 15:29:52 -0600

    update credit card expiration date attribute

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :transaction do
     invoice nil
     credit_card_number 1
-    credit_card_expiration "2018-08-14 12:22:31"
+    credit_card_expiration_date "2018-08-14 12:22:31"
     result "MyString"
   end
 end
