cb62bfd5a67b7b2089f1f36f08a452688862672d
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-14 15:37:40 -0600

    request spec for transaction show

--- spec/requests/transaction_request_spec.rb
+++ spec/requests/transaction_request_spec.rb
@@ -12,9 +12,29 @@ describe "Transactions API" do
       transactions = JSON.parse(response.body, symbolize_names: true)
       transaction = transactions.first
 
-      expect(response).to have_http_status(200)
       expect(transactions.count).to eq(3)
       expect(transaction).to have_key(:id)
+      expect(transaction).to have_key(:invoice_id)
+      expect(transaction).to have_key(:result)
+      expect(transaction).to_not have_key(:credit_card_number)
+      expect(transaction).to_not have_key(:credit_card_expiration_date)
+      expect(transaction).to_not have_key(:created_at)
+      expect(transaction).to_not have_key(:updated_at)
+    end
+  end
+
+   context "GET /api/v1/transactions/:id" do
+    it "returns single transaction" do
+      create_list(:transaction, 3)
+      transact = Transaction.first
+      
+      get "/api/v1/transactions/#{transact.id}.json"
+      
+      expect(response).to be_successful
+      
+      transaction = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(transaction[:id]).to eq(transact.id)
       expect(transaction).to have_key(:invoice_id)
       expect(transaction).to have_key(:result)
       expect(transaction).to_not have_key(:credit_card_number)
