de6afd97997649b1720d1ce13f6cf110554cda01
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-14 19:15:45 -0600

    fixing end problem

--- spec/requests/invoice_request_spec.rb
+++ spec/requests/invoice_request_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe "Invoices API" do
-  it "sends a list of invoices" do
-    create_list(:invoice, 3)
+  context 'get /api/v1/invoices' do
+    it "sends a list of invoices" do
+      create_list(:invoice, 3)
 
       get "/api/v1/invoices.json"
 
