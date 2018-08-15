bc749bf4a3a28d24a0fab1b6348f9a6e2753edaa
Stephen Malek <malek.stephen44@gmail.com>
2018-08-14 20:33:27 -0600

    create invoice items show

--- spec/requests/invoice_item_request_spec.rb
+++ spec/requests/invoice_item_request_spec.rb
@@ -23,4 +23,23 @@ describe "Invoices API" do
       expect(invoice_item).to_not have_key(:updated_at)
     end
   end
+  context 'get /api/v1/invoice_items/:id' do
+    it "sends a list of invoice_items" do
+      create_list(:invoice_item, 3)
+      inv_itm = Item.first
+      get "/api/v1/invoice_items/#{inv_itm.id}.json"
+
+      expect(response).to be_successful
+
+      invoice_item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoice_item).to have_key(:id)
+      expect(invoice_item).to have_key(:item_id)
+      expect(invoice_item).to have_key(:invoice_id)
+      expect(invoice_item).to have_key(:quantity)
+      expect(invoice_item).to have_key(:unit_price)
+      expect(invoice_item).to_not have_key(:created_at)
+      expect(invoice_item).to_not have_key(:updated_at)
+    end
+  end
 end
