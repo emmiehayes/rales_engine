c0a84453d699ad007a14a09615ba818eb5d222ca
Stephen Malek <malek.stephen44@gmail.com>
2018-08-15 15:02:39 -0600

    merchant revenue wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+
+  def total_revenue
+    invoices
+      .joins(:transactions, :invoice_items)
+      .where(transactions: {result: 'success'})
+      .sum("invoice_items.quantity*invoice_items.unit_price")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,4 +3,13 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   it {should have_many :invoices}
   it {should have_many :items}
+
+  describe "instance methods" do
+    it "can find the total revenue for one merchant" do
+      merchant = Merchant.create!(name: 'merch')
+      customer = Customer.create!(first_name: 'bob', last_name: 'jim')
+      merchant.invoices.create(customer_id: customer.id, status: 'something')
+
+    end
+  end
 end
