b04713008e464b815ceef312b98e6303c7463896
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-16 11:39:39 -0600

    round out relationship testing

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it {should have_many :invoices}
-  it {should have_many :items}
+  it { should have_many :invoices }
+  it { should have_many :items }
+  it { should have_many(:invoice_items).through(:invoices) }
+  it { should have_many(:transactions).through(:invoices) }
+  it { should have_many(:customers).through(:invoices) }
 
   context 'instance methods' do
     it '.favorite_customer' do 
