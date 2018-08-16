81eac89c504117928d5d59eff35a30d312b3d5f8
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-16 11:07:11 -0600

    complete endpoint for favorite customer for a merchant

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,10 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/most_revenue', to: 'most_revenue#index'
       end
-
+      
       resources :merchants, only: [:index, :show] do
         get '/items', to: 'merchants/items#show'
+        get '/favorite_customer', to: 'merchants/favorite_customer#show'
       end
 
       namespace :transactions do
